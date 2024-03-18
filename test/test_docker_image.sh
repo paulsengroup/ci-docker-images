17d5d6ab33d721b5cb47f19ad7a7534d6e15a24d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-03-18 12:02:49 +0100

    Test CXX image before pushing

---
+++ test/test_docker_image.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -eu
+set -o pipefail
+
+if [ $# -ne 1 ]; then
+  2>&1 echo "Usage: $0 image:latest"
+  exit 1
+fi
+
+IMG="$1"
+
+tmpdir="$(mktemp -d)"
+# shellcheck disable=SC2064
+trap "rm -rf '$tmpdir'" EXIT
+
+cat > "$tmpdir/runme.sh" <<- 'EOM'
+
+set -eu
+
+# print distro information
+cat /etc/*-release
+
+# print compiler information
+cc --version
+c++ --version
+
+# print CMake information
+which cmake
+cmake --version
+
+# test Conan
+printf '[requires]\nfmt/10.2.1' > conanfile.txt
+conan install --build=missing \
+      -pr:b="$CONAN_DEFAULT_PROFILE_PATH" \
+      -pr:h="$CONAN_DEFAULT_PROFILE_PATH" \
+      conanfile.txt
+EOM
+
+chmod 755 "$tmpdir/runme.sh"
+
+sudo docker run --rm --entrypoint=/bin/bash \
+  -v "$tmpdir/runme.sh:/tmp/runme.sh:ro" \
+  "$IMG" \
+  /tmp/runme.sh
